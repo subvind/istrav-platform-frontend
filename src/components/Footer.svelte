1bd20c72c65bb6052ac0400ebf9b8824ca7da405
istrav <travis.burandt@gmail.com>
2022-08-06 18:01:50 -0500

    stream

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,40 +1,15 @@
-<script>
-  import { onMount } from "svelte";
-  import { parseJwt } from '../parseJwt'
-
-  export let active = 'activity-feed'
-  let authenticated = false
-  let token = {
-    id: '',
-    email: ''
-  }
-
-  onMount(() => {
-    let lsToken = localStorage.getItem('token')
-    if (lsToken) {
-      token = parseJwt(lsToken)
-    }
-
-    if (token.email) {
-      authenticated = true
-    } else {
-      authenticated = false
-    }
-  })
-</script>
-
 <footer>
   <nav class="white black-text">
     <div class="container">
       <div class="nav-wrapper">
         <ul class="">
           <li class=""><a href="https://istrav.net" target="_blank">/istrav/ © {new Date().getFullYear()}.</a></li>
-          <li class=""><a href="https://istrav.com" target="_blank">Made with ♥ by istrav.com.</a></li>
+          <li class=""><a href="https://istrav.com" target="_blank">Made with &hearts; by istrav.com.</a></li>
         </ul>
         <ul class="right">
-          <li><a href={`/contact-us`}>Contact Us</a></li>
-          <li><a href={`/privacy-policy`}>Privacy Policy</a></li>
-          <li><a href={`/terms-and-conditions`}>Terms & Conditions</a></li>
+          <li><a href={`https://istrav.com`}>Website</a></li>
+          <li><a href={`https://stream.istrav.dev`}>Stream</a></li>
+          <li><a href={`https://istrav.dev`}>Headquarters</a></li>
         </ul>
       </div>
     </div>
